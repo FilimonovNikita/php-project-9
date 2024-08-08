0eb84937a297b0061260e50b69741c83e38f34e2
Nikita Filimonov <filimonovnikitav@gmail.com>
2024-08-08 12:45:27 +0300

    step 1

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+
+session_start();
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome to Slim!');
+});
+
+$app->run();
